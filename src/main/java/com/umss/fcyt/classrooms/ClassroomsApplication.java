2a390e9ac0ff0f21330d6956834525a39ee64eed
JhonnyHerbas <jerbasjhon@gmail.com>
2024-03-16 10:35:27 -0400

    initial commit

---
+++ src/main/java/com/umss/fcyt/classrooms/ClassroomsApplication.java
@@ -0,0 +1,13 @@
+package com.umss.fcyt.classrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassroomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassroomsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umss/fcyt/classrooms/ClassroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umss.fcyt.classrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
